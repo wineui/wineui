c961cdae960015481d740feb1dc00f892d3c87a7
Ridho Prasetya <ndiing@me.com>
2013-11-09 16:45:56 +0700

    added wine ui test

---
+++ scss/config.rb
@@ -0,0 +1,8 @@
+# wine ui
+#------------------------------------------------
+dir           = File.dirname(__FILE__)
+css_path      = File.join(dir, '..', 'assets', 'css')
+sass_path     = dir
+output_style  = :compressed
+line_comments = false
+cache         = false
